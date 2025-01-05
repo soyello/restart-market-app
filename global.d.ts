d5b5f46fd7b0afd7d1b5db616174370c6008fd3e
soyello <wodus9193@naver.com>
2025-01-05 20:57:07 +0900

    Kakao Map 컴포넌트 생성

---
+++ global.d.ts
@@ -0,0 +1,18 @@
+export {};
+
+declare global {
+  namespace kakao {
+    namespace maps {
+      class LatLng {
+        getLat(): number;
+        getLng(): number;
+      }
+
+      namespace event {
+        interface MouseEvent {
+          latLng: LatLng;
+        }
+      }
+    }
+  }
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,34 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { SessionProvider } from 'next-auth/react';
+import { createContext, useEffect, useState } from 'react';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const KakaoContext = createContext<{ isKakaoLoaded: boolean }>({ isKakaoLoaded: false });
+
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const [isKakaoLoaded, setIsKakaoLoaded] = useState(false);
+
+  useEffect(() => {
+    if (window.kakao && window.kakao.maps) {
+      setIsKakaoLoaded(true);
+    }
+  });
   return (
     <SessionProvider session={session}>
-      <div className={inter.className}>
-        <Navbar />
-        <Component {...pageProps} />;
-      </div>
+      <KakaoContext.Provider value={{ isKakaoLoaded }}>
+        <div className={inter.className}>
+          <Navbar />
+          <Component {...pageProps} />;
+          <Script
+            src='//dapi.kakao.com/v2/maps/sdk.js?appkey=9268d3bcf6b80dc4ae2dd0de7e26caab&libraries=services,clusterer&autoload=false'
+            strategy='lazyOnload'
+            onLoad={() => setIsKakaoLoaded(true)}
+          />
+        </div>
+      </KakaoContext.Provider>
     </SessionProvider>
   );
 }
