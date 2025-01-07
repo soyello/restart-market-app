a0ae3021a702e1c93ce8c7aae5496e4cabaa5f5f
soyello <wodus9193@naver.com>
2025-01-07 23:08:33 +0900

    useSWRMutation

--- src/components/chat/Input.tsx
+++ src/components/chat/Input.tsx
@@ -2,20 +2,48 @@ import axios from 'axios';
 import React, { FormEvent, useState } from 'react';
 import { IoImageOutline } from 'react-icons/io5';
 import { RiSendPlaneLine } from 'react-icons/ri';
+import useSWRMutation from 'swr/mutation';
 
 interface InputProps {
   receiverId: string;
   currentUserId: string;
 }
+const sendRequest = async (
+  url: string,
+  {
+    arg,
+  }: {
+    arg: {
+      text: string;
+      image: string;
+      receiverId: string;
+      senderId: string;
+    };
+  }
+) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(arg),
+  }).then((res) => {
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return res.json();
+  });
+};
 
 const Input = ({ receiverId, currentUserId }: InputProps) => {
   const [message, setMessage] = useState('');
+  const { trigger } = useSWRMutation('/api/chat', sendRequest);
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const imageUrl = '';
     if (message || imageUrl) {
       try {
-        await axios.post('/api/chat', {
+        trigger({
           text: message,
           image: imageUrl,
           receiverId: receiverId,
